bdc8ecab37d3f8353eaa08806b4ad66a3ea1f1ad
Jeff Moody <jeff@fifthecho.com>
2021-03-26 09:20:48 -0400

    Update Sensu Go Version

--- sensu-go-agent/plan.sh
+++ sensu-go-agent/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=sensu-go-agent
 pkg_origin=kepler
-pkg_version="6.2.3"
+pkg_version="6.2.6"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
 pkg_source="https://github.com/sensu/sensu-go/archive/v${pkg_version}.tar.gz"
-pkg_shasum="13207680bf6ed7b105e76cb94cf3cc886e3e34f73748a7d96cf566817dd9814a"
+pkg_shasum="1714d92dc1a6a639ad6c3aa9372cb09d2a8eb772e3983eae8416111b53a94197"
 pkg_deps=(
   core/glibc
 )
